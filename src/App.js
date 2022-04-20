302aa060bd86a7aff36b7e2ad5c99b9fd44d392d
Sumitra Shrestha <sumiitrashrestha@gmail.com>
2022-04-20 22:27:37 +0545

    useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React,{useState} from 'react';
+import React,{useState}from 'react'
 import './App.css';
-import PostsPage from './PostsPage';
-
-
 function App() {
-
+  const [count,setCount]=useState(0);
+  const[showCount,setShowCount]=useState(true);
+  const[guitar,setGuitar]=useState({
+    brand:'Fender',
+    model:'Stratocaster'
+  })
+  fetch(`api.com/${song}`).then(response=>setSong({...song,artist:response.artist,song:response.song}))
   return (
-    <div className='App'>
+    <div className="App">
+    <header className="App-header">
+    {/*
+    <button onClick={()=>setShowCount(!showCount)}>toggle count</button>
 
-   <PostsPage/>
-    </div>
+    <button onClick={()=>setCount(count+1)}>Count it</button>
+
+    {showCount?<p>{count}</p>:null}*/}
+    <button onClick={()=>setGuitar({...guitar,model:'Telecaster'})}>Change model</button>
+    
+     <p>{guitar.brand}</p>
+     <p>{guitar.model}</p>
+   
+
+    </header></div>
   );
 }
-
-export default App
+export default App;
