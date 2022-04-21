fa3df004917abc25271795ef2bc9d273bd226843
Sumitra Shrestha <sumiitrashrestha@gmail.com>
2022-04-21 13:44:28 +0545

    filterNames

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import useFetch from './useFetch';
+import{useState}from 'react';
 import './App.css';
-
-function App() {
-  const{data:quote,loading,error}=useFetch('https://api.quotable.io/random');
-
-
-  return (
-    <div className='App'>
-   {loading&&<p>{loading}</p>}
-   {quote&&<p>"{quote}"</p>}
-   {error&&<p>{error}</p>}
-    
+function App(){
+  const namesFromDatabase=[
+    {id:1,username:'Sumitra Shrestha'},
+    {id:2,username:'Nikita Rijal'},
+    {id:3,username:'  Nabina Kusi'}
+  ]
+  const[names,setNames]=useState(namesFromDatabase)
+  const filterNames=e=>{
+    const search=e.target.value.toLowerCase()
+    const filteredNames=namesFromDatabase.filter(names=>names.username.toLowerCase().includes(search))
+    setNames(filteredNames)
+  }
+  return(
+    <div className="App">
+      <header className="App-header">
+        <h2>Username</h2>
+        <hr/>
+        <input type='text'onChange={(e)=>filterNames(e)}/>
+        <button>Search</button>
+        <ul>
+          {names.map(name=>{
+            return<li key={name.id}>
+              {name.username}</li>
+          
+          })}
+        </ul>
+      </header>
     </div>
-  
-  );
+  )
 }
-
 export default App
