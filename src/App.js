b581253eb7259ab7fafbe553903b3ee324ccb7e4
Sumitra Shrestha <sumiitrashrestha@gmail.com>
2022-04-24 11:40:02 +0545

    useState

--- src/App.js
+++ src/App.js
@@ -1,78 +1,24 @@
-import React,{useState,useEffect,useRef} from 'react';
-import FlashcardList from './FlashcardList';
-import './app.css';
-import axios from 'axios'
+import React,{useState} from 'react'
 
- function App() {
-   const[flashCards,setFlashCards]=useState([])
-   const[categories,setCategories]=useState([])
-   const categoryEl=useRef()
-   const amountEl=useRef()
-
-   useEffect(()=>{
-     axios.get('https://opentdb.com/api_category.php')
-     .then(res=>{
-setCategories(res.data.trivia_categories)
-     })
-
-   },[])
-
-   useEffect(()=>{
-
-    },[])
-
-function decodeString(str){
-  const textarea=document.createElement('textarea')
-  textarea.innerHTML=str
-  return textarea.value
-}
-function handleSubmit(e){
-  e.preventDefault()
-  axios.get('https://opentdb.com/api.php',{
-    params:{
-      amount:amountEl.current.value,
-      category:categoryEl.current.value
+function App() {
+  const[count,setCount]=useState(4)
+  const[theme,setTheme]=useState('blue')
+    function  decrementCount(){
+setCount(prevCount=>prevCount-1)
 
     }
-  })
-.then(res=>{
-  setFlashCards(res.data.results.map((questionItem,index)=>{
-    const answer=decodeString(questionItem.correct_answer)
-   const options=[...questionItem.incorrect_answers
-    .map(a=>decodeString(a)),answer]
-    return{
-      id:`${index}+${Date.now()}`,    question:decodeString(questionItem.question),
-      answer:answer,
+    function  incrementCount(){
+setCount(prevCount=>prevCount+1)
+setTheme('red')
+  }
+  return (
 
-options:options.sort(()=>Math.random()-.5)
-    }
-  }))
-})
- 
-  
-}
-  return (<>
-  <form className='header'onSubmit={handleSubmit}>
-  <div className='form-group'>
-    <label htmlFor='category'>Category</label>
-    <select id='category' ref={categoryEl}>
-    {categories.map(category=>{
-      return <option value={category.id}key={category.id}>{category.name}</option>
-    })}
-
-    </select>
-  </div>
-  <div className='form-group'>
-   <label htmlFor='amount'>Number of Questions</label>
-   <input type='number'id='amount'min='1'step='1'defaultValue={10}ref={amountEl}/>
-</div>
-  <div className='form-group'>
-    <button className='btn'>Generate</button>
-  </div>
-  </form><div className='container'>
-<FlashcardList flashCards={flashCards}/>
-</div></>
+<>
+  <button onClick={decrementCount}>-</button>
+  <span>{count}</span>
+    <span>{theme}</span>
+  <button onClick={incrementCount}>+</button>
+</>
   )
 }
-
 export default App
