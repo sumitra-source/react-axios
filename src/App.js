4fa19cc6d8dfd6f489954ecce0d72ff84216c25e
Sumitra Shrestha <sumiitrashrestha@gmail.com>
2022-04-24 18:58:36 +0545

    todo app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,90 @@
-import React ,{useState}from 'react'
-import FunctionContextComponent from './FunctionContextComponent'
+import React,{useReducer,useState} from 'react'
+import Todo from './Todo'
+export const ACTIONS={
+  ADD_TODO:"add todo",
+  TOGGLE_TODO:"toggle todo",
+  DELETE_TODO:"delete todo"
 
-import {ThemeProvider} from './ThemeContext';
+}
+function reducer(todos,action){
+  switch(action.type){
+    case ACTIONS.ADD_TODO:
+      return[...todos,newTodo(action.payload.name)]
+      case ACTIONS.TOGGLE_TODO:
+        return todos.map(todo=>{
+          if(todo.id===action.payload.id){
+            return{...todo,complete: !todo.complete}
+          }
+          return todo
+        })
+        case ACTIONS.DELETE_TODO:
+        return todos.filter(todo=>todo.id!=action.payload.id)
+        
+        default:
+          return todos
+             }
 
- function App() {
- 
- 
-  return (<>
-  <ThemeProvider >
-<FunctionContextComponent/>
+}
+function newTodo(name){
+  return{id:Date.now(),name:name,complete:false}
 
-</ThemeProvider>    </>
+}
+
+export default function App() {
+  const[todos,dispatch]=useReducer(reducer,[])
+  const [name,setName]=useState('')
+
+  function handleSubmit(e){
+    e.preventDefault()
+    dispatch({type:ACTIONS.ADD_TODO,payload:{name:name}})
+    setName('')
+  }
+  console.log()
+  return (
+    <>
+<form onSubmit={handleSubmit}>
+  <input type='text' value={name}onChange={e=>setName(e.target.value)}/>
+</form>
+{todos.map(todo=>{
+  return<Todo key={todo.id} todo={todo} dispatch={dispatch}/>
+})}
+    </>
   )
 }
 
-export default App
+
+
+
+{/*import React,{useReducer} from 'react';
+const ACTION={
+  INCREMENT:'increment',
+  DECREMENT:'decrement'
+}
+function reducer(state,action){
+  switch(action.type){
+    case ACTION.INCREMENT:
+      return{count:state.count+1}
+      case ACTION.DECREMENT:
+  return{count:state.count-1}
+  default:
+    return state
+  }
+}
+function App() {
+    const[state,dispatch]=useReducer(reducer,{count:0})
+ function  incrementCount(){
+     dispatch({type:ACTION.INCREMENT})}
+  function  decrementCount(){
+dispatch({type:ACTION.DECREMENT})}
+
+  return (
+
+<>
+  <button onClick={decrementCount}>-</button>
+  <span>{state.count}</span>
+  
+  <button onClick={incrementCount}>+</button>
+</>
+  )
+}
+export default App*/}
