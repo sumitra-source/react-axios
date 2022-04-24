3f164534cc8a00fd454f2002c0101ca89de90974
Sumitra Shrestha <sumiitrashrestha@gmail.com>
2022-04-24 12:24:54 +0545

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 
-function App() {
-  const[count,setCount]=useState(4)
-  const[theme,setTheme]=useState('blue')
-    function  decrementCount(){
-setCount(prevCount=>prevCount-1)
+{/*function App() {
+  const[resourceType,setResourceType]=useState('posts')
+  const[items,setItems]=useState([])
 
-    }
-    function  incrementCount(){
-setCount(prevCount=>prevCount+1)
-setTheme('red')
-  }
+  console.log('render')
+
+  useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+    .then(response=>response.json())
+    .then(json=>setItems(json))
+
+  },[resourceType])
+
   return (
 
-<>
-  <button onClick={decrementCount}>-</button>
-  <span>{count}</span>
-    <span>{theme}</span>
-  <button onClick={incrementCount}>+</button>
+<><div>
+  <button onClick={()=>setResourceType('posts')}>Posts</button>
+ <button onClick={()=>setResourceType('users')}>Users</button>
+ <button onClick={()=>setResourceType('comments')}>Comments</button>
+ </div>
+ <h1>{resourceType}</h1>
+ {items.map(item=>{
+   return<pre>{JSON.stringify(item)}</pre>
+ })}
 </>
+
   )
-}
+}*/}
+function App(){
+  const[windowWidth,setWindowWidth]=useState(window.innerWidth)
+  const handleResize=()=>{
+    setWindowWidth(Window.innerWidth)
+  }
+  
+  useEffect(()=>{
+    window.addEventListener('resize',handleResize)
+    return()=>{
+      window.removeEventListener('resize',handleResize)
+    }
+  },[])
+  return(
+    <div>{windowWidth}</div>
+  )}
+
 export default App
