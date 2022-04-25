de52d0a2ac66989688755ac803f3c71a678a8977
Sumitra Shrestha <sumiitrashrestha@gmail.com>
2022-04-25 12:15:03 +0545

    customHooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React,{useState,useCallback} from 'react'
-import List from './List'
+
+import React from 'react'
+import useLocalStorage from './useLocalStorage'
+import useUpdateLogger from './useUpdateLogger'
 
 export default function App() {
-  const[number,setNumber]=useState(1)
-  const [dark,setDark]=useState(false)
-  const getItems=useCallback((incrementer)=>{
-    return[number+incrementer,number
-    +1+incrementer,number+2+incrementer]
-  },[number])
-  const theme={
-    backgroundColor:dark?'#333':'#FFF',
-    color:dark?'#FFF':'#333'
-  }
+  const[name,setName]=useLocalStorage('name','')
+  useUpdateLogger(name)
   return (
-    <div style={theme}>
-    <input type="number"value={number} onChange={e=>setNumber(parseInt(e.target.value))}/>
-    <button onClick={()=>setDark(prevDark=>!prevDark)}>
-      ToggleTheme
-    </button>
-    <List getItems={getItems}/>
-    </div>
+  <input type="text"value={name}onChange={e=>setName(e.target.value)}/>
   )
 }
